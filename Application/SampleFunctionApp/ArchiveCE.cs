0cca83e207482b9439fcf295c6081f00bcc1e607
nick Hill <nick-9979@outlook.com>
2019-01-25 15:17:01 +0000

    chnage to use SAS token to access ceBlob and improve logging

--- Application/SampleFunctionApp/ArchiveCE.cs
+++ Application/SampleFunctionApp/ArchiveCE.cs
@@ -37,16 +37,18 @@ namespace SampleFunctionApp
             var continuousExportStorageConnectionString = config["ContinuousExportStorageConnectionString"];
             if (continuousExportStorageConnectionString == null)
             {
-                log.LogCritical(@"Variable ContinuousExportStorageConnectionString is not set in configuration.  This function cannot run. ");
-                return;
+                string m = @"Variable ContinuousExportStorageConnectionString is not set in configuration.  This function cannot run.";
+                log.LogCritical(m);
+                throw new Exception(m);
             }
             log.LogInformation($"CE export storage connection string: {continuousExportStorageConnectionString.Substring(0, 25)}");
 
             var archiveStorageConnectionString = config["ArchiveStorageConnectionString"];
             if (archiveStorageConnectionString == null)
             {
-                log.LogCritical(@"Variable ArchiveStorageConnectionString is not set in configuration.  This function cannot run. ");
-                return;
+                string m = @"Variable ArchiveStorageConnectionString is not set in configuration.  This function cannot run.";
+                log.LogCritical(m);
+                throw new Exception(m);
             }
             log.LogInformation($"Archive storage connection string: {archiveStorageConnectionString.Substring(0, 25)}");
 
@@ -78,45 +80,70 @@ namespace SampleFunctionApp
             {
                 blobName += u.Segments[i];
             }
-            log.LogInformation(($"Blob Name: {blobName}"));
+            log.LogInformation($"Blob Name: {blobName}");
 
             
             try
             {
+                log.LogInformation($"Get hold of ce storage container");
                 CloudStorageAccount ceStorageAccount = CloudStorageAccount.Parse(continuousExportStorageConnectionString);
                 CloudBlobClient ceStorageClient = ceStorageAccount.CreateCloudBlobClient();
                 CloudBlobContainer ceStorageContainer = ceStorageClient.GetContainerReference(ceContainerName);
+                log.LogInformation($"ceStorageContainer.Uri: {ceStorageContainer.Uri}");
 
+                log.LogInformation($"Get hold of archive storage container");
                 CloudStorageAccount archiveStorageAccount = CloudStorageAccount.Parse(archiveStorageConnectionString);
                 CloudBlobClient archiveStorageClient = archiveStorageAccount.CreateCloudBlobClient();
-                CloudBlobContainer archiveStorageContainer = archiveStorageClient.GetContainerReference("archive");
-                if (await archiveStorageContainer.CreateIfNotExistsAsync())
+                CloudBlobContainer archiveStorageContainer = archiveStorageClient.GetContainerReference(archiveContainerName);
+                if (await archiveStorageContainer.CreateIfNotExistsAsync().ConfigureAwait(false))
                 {
                     log.LogInformation(($"Archive container {archiveContainerName} did not exist and was created sucessfully."));
                 }
                 archiveStorageContainer.Metadata.Add("origin", "Create automatically by ArchiveCE Function");
                 await archiveStorageContainer.SetMetadataAsync();
+                log.LogInformation($"archiveStorageContainer.Uri: {ceStorageContainer.Uri}");
 
                 CloudBlockBlob ceBlob = ceStorageContainer.GetBlockBlobReference(blobName);
+                log.LogInformation($"ceBlob.Uri: {ceBlob.Uri}");
+                var policy = new SharedAccessBlobPolicy
+                {
+                    Permissions = SharedAccessBlobPermissions.Read,
+                    SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-15),
+                    SharedAccessExpiryTime = DateTime.UtcNow.AddDays(7)
+                };
+                log.LogInformation($"sharedAccessPolicy: {policy.ToString()}");
+                var ceBlobToken = ceBlob.GetSharedAccessSignature(policy);
+                log.LogInformation($"ceBlobToken: {ceBlobToken.ToString()}");
+                var ceBlobSAS = string.Format("{0}{1}", ceBlob.Uri, ceBlobToken);
+                log.LogInformation($"ceBlobSAS: {ceBlobSAS.ToString()}");
+
                 CloudBlockBlob archiveBlob = archiveStorageContainer.GetBlockBlobReference(blobName);
-                log.LogInformation(($"Set storage tier for archive blob to 'archive'"));
+                log.LogInformation($"archiveBlob.Uri: {archiveBlob.Uri}");
+                log.LogInformation($"Set storage tier for archive blob to 'archive'");
                 await archiveBlob.SetStandardBlobTierAsync(StandardBlobTier.Archive);
-                log.LogInformation(($"Set storage tier for archive blob to 'archive'"));
+
+                log.LogInformation(($"set traceability in archive blob metadata"));
                 archiveBlob.Metadata.Add("traceability", $"archived by ArchiveCE Azure function at {System.DateTime.UtcNow} UTC)");
                 await archiveBlob.SetMetadataAsync();
+
                 log.LogInformation(($"copying blob to archive account {archiveStorageAccount.BlobStorageUri}, archive container {archiveContainerName}"));
-                string result = await archiveBlob.StartCopyAsync(ceBlob);
-                log.LogInformation(($"Result of Blob copy operation: {result}."));
+                string result = await archiveBlob.StartCopyAsync(new Uri(ceBlobSAS)).ConfigureAwait(false);
+                log.LogInformation($"Result of Blob copy operation: {result}.");
+
+                
+                
+
             }
             catch (Exception e)
             {
                 log.LogError($"Unable to archive blob Name: {blobName}.  Exception was {e.Message}");
-                log.LogError($"Unable to archive blob Name: {blobName}.  Exception was {e.StackTrace}");
+                log.LogError($"Unable to archive blob Name: {blobName}.  Stacktrace was {e.StackTrace}");
                 if (e.InnerException != null)
                 {
                     log.LogError($"Unable to archive blob Name: {blobName}.  Inner Exception was {e.InnerException.Message}");
-                    log.LogError($"Unable to archive blob Name: {blobName}.  Exception was {e.InnerException.StackTrace}");
+                    log.LogError($"Unable to archive blob Name: {blobName}.  Stacktrace was {e.InnerException.StackTrace}");
                 }
+                throw e;
             }
 
            
